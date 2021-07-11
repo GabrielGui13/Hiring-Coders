b9898713eec1ef9725d982c87496146025fb17b1
GabrielGui13 <gabrielguilherme13@hotmail.com>
2021-07-11 18:17:02 -0300

    to do list finalizado

--- Exercicios-Javascript/to do list/script.js
+++ Exercicios-Javascript/to do list/script.js
@@ -1,9 +1,27 @@
 let botao = document.getElementById('add')
+let form = document.querySelector('[data-new-list-form]')
+let input = document.querySelector('[data-new-list-input]')
+let list = document.querySelector('[data-lists]')
 
-function adicionarElemento() {
-    let input = document.querySelector('[data-new-list-input]')
+window.addEventListener('onkeypress', event => {
+    event.preventDefault()
+
+    if (event.keyCode === 13) {
+        form.submit()
+    }
+})
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    inserirTexto()
+
+})
+
+function inserirTexto() {
     let texto = input.value
-    console.log(texto)
+    if (texto.trim() === '') return
+    var li = document.createElement('li')
+    li.innerHTML = texto
+    list.appendChild(li)
+    input.value = ''
 }
-
-botao.addEventListener('click', adicionarElemento)
